81d8a8c1e7d7a8890ad61bf16a0867626befce36
jcmourey <jean-charles@mourey.com>
2024-06-08 18:17:43 +0200

    Initial Commit

---
+++ JourneyJar/JourneyJarApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyJarApp.swift
+//  JourneyJar
+//
+//  Created by Jean-Charles Mourey on 08/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct JourneyJarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
